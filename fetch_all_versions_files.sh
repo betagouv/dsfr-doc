d165ca566841aea868c3989a6ca45f29d1fcc3e3
Adrien Di Pasquale <adrien@dipasquale.fr>
2024-12-09 18:58:03 +0100

    fix fetch all versions

--- fetch_all_versions_files.sh
+++ fetch_all_versions_files.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-versions=$(jq -r '.versions[]' package.json)
+versions=$(jq -c '.versions[]' package.json)
 
 for version in $versions; do
+  version_number=$(echo $version | jq -r '.number')
+  has_storybook=$(echo $version | jq -r '.storybook')
+
+  printf "\nFetching files for version $version_number - Storybook: $has_storybook\n"
   mkdir -p "dist/v$version"
-  npm install --no-save "@gouvfr/dsfr@$version"
+  npm install --no-save "@gouvfr/dsfr@$version_number"
 
-  cp -r "node_modules/@gouvfr/dsfr/dist" "dist/v$version/"
-  cp -r "node_modules/@gouvfr/dsfr/example" "dist/v$version/"
-  cp -r "node_modules/@gouvfr/dsfr/doc" "dist/v$version/"
+  cp -r "node_modules/@gouvfr/dsfr/dist" "dist/v$version_number/"
+  cp -r "node_modules/@gouvfr/dsfr/example" "dist/v$version_number/"
+  cp -r "node_modules/@gouvfr/dsfr/doc" "dist/v$version_number/"
 
-  if [ -d "node_modules/@gouvfr/dsfr/storybook" ]; then
-    cp -r "node_modules/@gouvfr/dsfr/storybook" "dist/v$version/"
+  if [ "$has_storybook" = "true" ]; then
+    cp -r "node_modules/@gouvfr/dsfr/storybook" "dist/v$version_number/"
   fi
-
 done
