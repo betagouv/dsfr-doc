a46e6518e96588510397e31cbddbb7e374855f8a
Adrien Di Pasquale <adrien@dipasquale.fr>
2024-12-09 18:38:40 +0100

    store versions numbers in package.json

--- fetch_all_versions_files.sh
+++ fetch_all_versions_files.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-# Define the versions you want to iterate over
-versions=("1.12.1" "1.13.0")
+versions=$(jq -r '.versions[]' package.json)
 
-for version in "${versions[@]}"; do
+for version in $versions; do
   mkdir -p "dist/v$version"
   npm install --no-save "@gouvfr/dsfr@$version"
+
   cp -r "node_modules/@gouvfr/dsfr/dist" "dist/v$version/"
   cp -r "node_modules/@gouvfr/dsfr/example" "dist/v$version/"
+  cp -r "node_modules/@gouvfr/dsfr/doc" "dist/v$version/"
 
-  # Test if storybook exists
   if [ -d "node_modules/@gouvfr/dsfr/storybook" ]; then
     cp -r "node_modules/@gouvfr/dsfr/storybook" "dist/v$version/"
   fi
 
-  cp -r "node_modules/@gouvfr/dsfr/doc" "dist/v$version/"
 done
